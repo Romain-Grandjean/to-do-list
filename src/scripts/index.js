e2e52a6ca7e2d6fcb7be6b2673728625fe5c4ad4
Romain GRANDJEAN <romaingrandjean@Romains-Air.fritz.box>
2020-04-28 14:25:05 +0200

    version with enter key

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,7 @@
 NoText = () => {
   return alert("You need to enter a task");
 }
-
-
-document.querySelector('.buttonAdd').addEventListener('click', () => {
-
+ Newtask = () => {
   let userInput = document.querySelector("#userText").value;
   let newP = document.createElement("span");
   newP.className = "tasksText";
@@ -16,7 +13,6 @@ document.querySelector('.buttonAdd').addEventListener('click', () => {
   }
 
   let tasks = document.querySelector(".tasks");
-
   let divResult = document.createElement("divResult");
   divResult.className = "divResult";
   tasks.appendChild(divResult);
@@ -31,13 +27,29 @@ document.querySelector('.buttonAdd').addEventListener('click', () => {
   //   divResult.remove();
   //   });
 
-
 deleteButton.addEventListener('click', () => {
     divResult.remove();
     });
 
 
   document.querySelector("#userText").value = "";
+
+ }
+
+document.querySelector('.buttonAdd').addEventListener('click', () => {
+  return Newtask();
 });
+
+function enter(e) {
+  if (e.keyCode == 13) {
+  return Newtask();
+}
+};
+
+document.querySelector('#userText').addEventListener('keypress', enter);
+
+
+
+
 
 
